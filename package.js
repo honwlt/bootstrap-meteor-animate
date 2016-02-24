618fb9287c2c1112388f20617fdeecaca78f76b1
honwlt <vhonnur@gmail.com>
2016-02-24 12:40:02 +0530

    amend

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'honmeteo:bootstrap-meteor-animate',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Bootstrap Animate library for Meteor',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/honwlt/bootstrap-meteor-animate.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+
+//  api.use('twbs:bootstrap@4.0.0-alpha2', 'client', {weak: true});
+  api.addFiles('animate.css/animate.css', 'client');
+  api.addFiles('bootstrap-meteor-animate.js', 'client');
+});
